e93384a5da8a9cc834fda5b559ec000e95c33580
Wilmer Quispe <wilmerquispe@gmail.com>
2021-02-25 20:59:30 -0500

    Added DefaultBanner Component

--- src/Components/Pages/Characters.jsx
+++ src/Components/Pages/Characters.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import DefaultBanner from '../Atoms/DefaultBanner';
 
 const Characters = () => {
     return (
-        <h1>Hola mundo</h1>
+        <DefaultBanner title="Characters" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus corrupti libero magnam voluptates nostrum molestiae nemo accusantium officia illo laborum ratione pariatur, perferendis adipisci quidem in nihil itaque neque mollitia?"/>
     );
 }
 
